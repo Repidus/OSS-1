5b1c38986fd21ca8f0ec2dfe3fc94d37eaedb1b9
REPIDUS <moondj123@naver.com>
2018-04-13 10:20:26 +0900

    first commit after forking

--- source.cpp
+++ source.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 8; i++)
+	for (int i = 2; i <= 8; i++)
 	{
 		for (int j = 1; j <= 9; j++)
 		{
 			cout << i << " x " << j << " = " << i * j << endl;
 		}
 		cout << endl;
+	}
+
+	for (int i = 1; i <= 9; i++)
+	{
+		cout << 9 << " x " << i << " = " << 9 * i << endl;
 	}
 }
